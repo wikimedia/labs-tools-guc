wikimedia/labs-tools-guc

commit beb5d5bfc4b0ecc19d57f70bc15c32f8df5f0c95

Implement wildcard features and misc spring cleaning
Enhancements:

* Implement wildcard matching. (Fixes T66499)

* Add italic style for edit summary comment.

* Show "0 edits found" instead of an empty page if a search
  found no results.

* Associate "IP address or user" label with the input field (clickable).

Bug fixes:

* Under each wiki domain heading, display edit count from the global revision search.
  Previously it displayed the edit count from local user table. This is  already
  queried for the "Global edit count", is more accurate, includes IP users, and
  is compatible with the "Pattern search" feature.

* Fix urls for Russian wikis. (Fixes T94351)
  Previously it would unconditionally strip the first 7 characters,
  assuming it always starts with "http://". This caused HTTPS-only wikis
  to produce invalid urls when GUC later prefixes "//" in front of the
  hostname: http:///ru.wikipedia.org.

* Only strip protocol of wikis with a canonical server on HTTP. Thuss preserving
  the HTTPS preference of HTTPS-only wikis.

* Fix timestamp to not be in the German "d.m.Y" format. Use "d M Y" instead.

* Add an action attribute to <form> so that the browser address bar
  doesn't keep showing the 'user' query string of the first result.

* css: Move '-moz' and '-webkit' before the unprefixed declarations.
  https://css-tricks.com/ordering-css3-properties/

Maintenance:

* Clean up time handling by using stftime() and date().

* Throw "No username or IP" error in GUC instea of WikiContribs.
  Previously, if the form was submitted without a non-whitespace value, it
  threw one "Missing user" error for each wiki. Now it stops before starting
  iteration.

* js: In setTimeout, pass the function by reference instead
  of eval() string.

* js: In setLines, append one pipe to the text node instead
  of re-creating the entire sequence each iteration.

* Remove unused HTML class names.
* Remove unused PHP functions.
* Consistently use single quotes where possible.

lb/guc.php
@@ -15,57 +15,115 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-class guc
-{
+class guc {
     private $app;
-    private $isIP;
-    private $hostname;
-    private $username;
+    private $user;
+    private $options;
+
+    private $isIP = false;
+    private $hostnames = array();
+    private $globalEditCount = 0;
+
+    private $data;
     private $wikis;
-    private $alloverCount = 0;
-    public $wikisCount = 0;
-    public $wikisWithEditscount = 0;
 
-    public function __construct(lb_app $app, $closedWikis = false, $justLastHour = false) {
+    public function __construct(lb_app $app, $user, $options = array()) {
         $this->app = $app;
-        $this->username = str_replace("_", " ", ucfirst(trim($_POST['user'])));
-        $wikis = $this->_getWikis($closedWikis);
-        $WikisCountribsCounter = $this->_getWikisWithContribs($wikis);
-        $this->wikisCount = count($wikis);
-        try {
-            $this->hostname = @gethostbyaddr($this->username);
-        } catch (Exception $e) {
-            $this->hostname = '';
-        }
-        $this->isIP = !!$this->hostname;
-        // Check if input is a IP
-        foreach ($wikis as &$wiki) {
-            if ($WikisCountribsCounter[$wiki['dbname']] > 0) {
-                // Strip http://
-                $wiki['url'] = mb_substr($wiki['url'], 7);
-                try {
-                    $wiki['data'] = new lb_wikicontribs($this->app, $this->username, $wiki['dbname'], $wiki['url'], $wiki['slice'], $wiki['family'], $this->_getCentralauthData($wiki['dbname']), $this->isIP, $justLastHour);
-                    $wiki['error'] = null;
-                } catch (Exception $e) {
-                    $wiki['data'] = null;
-                    $wiki['error'] = "Error with ".$wiki['url'].': '.$e->getMessage().'<br>';
-                }
-            } else {
-                // No contribs
-                $wiki['data'] = null;
-                $wiki['error'] = null;
-            }
 
+        // Normalise
+        $this->user = str_replace('_', ' ', ucfirst(trim($user)));
+
+        // Defaults
+        $this->options = $options += array(
+            'isPrefixPattern' => false,
+            'includeClosedWikis' => false,
+            'onlyRecent' => false,
+        );
+
+        if (!$this->user) {
+            throw new Exception('No username or IP');
         }
+
+        // Check if input is a pattern
+        if ($this->options['isPrefixPattern']) {
+            if (strpos($this->user, '_') !== false) {
+                throw new Exception('Illegal "_" character found');
+            }
+            // Pattern search must be prefix-based for performance
+            if (substr($this->user, 0, 1) === '%') {
+                throw new Exception('Wildcard search can not start with "%".');
+            }
+            // Hidden feature: User can specify "%" somewhere in the query.
+            // Though by default we'll assume a prefix search.
+
+            if (substr($this->user, -1) !== '%') {
+                $this->user .= '%';
+            }
+            $this->app->aTP('Perfoming a pattern search: ' . $this->user);
+        } else {
+            // Check if input is an IP
+            if ($this->addIP($this->user)) {
+                $this->isIP = true;
+            }
+        }
+
+        $wikis = $this->_getWikis();
+        // Filter down wikis to only relevant ones.
+        // Attaches 'wiki._editcount' property.
+        $wikisWithEditcount = $this->_getWikisWithContribs($wikis);
+
+        $datas = new stdClass();
+        foreach ($wikisWithEditcount as $dbname => $wiki) {
+            $wiki->canonical_server = $wiki->url;
+            $wiki->domain = preg_replace('#^https?://#', '', $wiki->canonical_server);
+            // Convert "http://" to "//".
+            // Keep https:// as-is since we should not override that. (phabricator:T94351)
+            $wiki->url = preg_replace('#^http://#', '//', $wiki->canonical_server);
+
+            $data = new stdClass();
+            $data->wiki = $wiki;
+            $data->error = null;
+            $data->contribs = null;
+
+            try {
+                $contribs = new lb_wikicontribs(
+                    $this->app,
+                    $this->user,
+                    $this->isIP,
+                    $wiki,
+                    $this->_getCentralauthData($wiki->dbname),
+                    $options
+                );
+                if ($this->options['isPrefixPattern'] && !$contribs->getRegisteredUsers()) {
+                    foreach ($contribs->getRecentChanges() as $rc) {
+                        $this->addIP($rc->rev_user_text);
+                        if (count($this->hostnames) > 10) {
+                            break;
+                        }
+                    }
+                }
+                $data->contribs = $contribs;
+            } catch (Exception $e) {
+                $wiki->error = $e;
+            }
+            unset($contribs);
+            $datas->$dbname = $data;
+        }
+
+        // List of all wikis
         $this->wikis = $wikis;
+        // Array of wikis with edit count, keyed by dbname
+        $this->wikisWithEditcount = $wikisWithEditcount;
+        // Contributions, keyed by dbname
+        $this->datas = $datas;
     }
 
     /**
-     * return all wikis
-     * @return mixed
+     * Get all wikis
+     * @return array of objects
      */
-    private function _getWikis($closedIncluded = false) {
-        $this->app->aTP('get wiki list..');
+    private function _getWikis() {
+        $this->app->aTP('Get list of all wikis');
         $family = array(
             'wikipedia' => 1,
             'wikibooks' => 1,
@@ -82,106 +140,155 @@ class guc
             'wikimania' => 1
         );
         $f_where = array();
-        if (!$closedIncluded) {
+        if (!$this->options['includeClosedWikis']) {
             $f_where[] = 'is_closed = 0';
         }
-        foreach ($family as $name => $include) {
-            if ($include == 0) {
+        foreach ($family as $name => $value) {
+            if ($value !== 1) {
                 $f_where[] = '`family` != \''.$name.'\'';
             }
         }
-        $f_where = implode(" AND ", $f_where);
-        $qry = 'SELECT * FROM `meta_p`.`wiki` WHERE '.$f_where.' LIMIT 1500;';
-        $con = $this->app->getDB()->prepare($qry);
-        $con->execute();
-        $r = $con->fetchAll();
-        unset($con);
-        return $r;
+        $f_where = implode(' AND ', $f_where);
+        $sql = 'SELECT * FROM `meta_p`.`wiki` WHERE '.$f_where.' LIMIT 1500;';
+        $statement = $this->app->getDB()->prepare($sql);
+        $statement->execute();
+        $rows = $statement->fetchAll(PDO::FETCH_OBJ);
+        unset($statement);
+        return $rows;
     }
 
     /**
      * return the wikis with contribs
-     * @param type $wikis
-     * @return type
+     * @param array $wikis
+     * @return array
      */
-    private function _getWikisWithContribs($wikis) {
-        $return = array();
+    private function _getWikisWithContribs(Array $wikis) {
+        $this->app->aTP('Query all wikis for matching revisions');
+        $wikisWithEditcount = array();
+
         $slices = array();
-        $wikisWithEditCount = 0;
+        $wikisByDbname = array();
         foreach ($wikis as $wiki) {
-            if (!is_array($slices[$wiki['slice']])) {
-                $slices[$wiki['slice']] = array();
-            }
-            $slices[$wiki['slice']][] = 'SELECT COUNT(rev_id) AS counter, \''.$wiki['dbname'].'\' AS dbname FROM '.$wiki['dbname'].'_p.revision_userindex WHERE rev_user_text = :username';
+            $wikisByDbname[$wiki->dbname] = $wiki;
+            $slices[$wiki->slice][] = 'SELECT
+                COUNT(rev_id) AS counter,
+                \''.$wiki->dbname.'\' AS dbname
+                FROM '.$wiki->dbname.'_p.revision_userindex
+                WHERE '.(
+                    ($this->options['isPrefixPattern'])
+                        ? 'rev_user_text LIKE :userlike'
+                        : 'rev_user_text = :user'
+                );
         }
-        foreach ($slices as $slicename => $sql) {
-            if ($sql) {
-                $sql = implode(' UNION ALL ', $sql);
-                $qry = $this->app->getDB('meta', $slicename);
-                $con = $qry->prepare($sql);
-                $con->bindParam(':username', $this->username);
-                $con->execute();
-                $r = $con->fetchAll(PDO::FETCH_ASSOC);
-                foreach ($r as $re) {
-                    $return[$re['dbname']] = intval($re['counter']);
-                    if ($re['counter'] > 0) {
-                        $wikisWithEditCount++;
+
+        $globalEditCount = 0;
+        foreach ($slices as $sliceName => $queries) {
+            if ($queries) {
+                $sql = implode(' UNION ALL ', $queries);
+                $pdo = $this->app->getDB('meta', $sliceName);
+                $statement = $pdo->prepare($sql);
+                if ($this->options['isPrefixPattern']) {
+                    $statement->bindParam(':userlike', $this->user);
+                } else {
+                    $statement->bindParam(':user', $this->user);
+                }
+                $statement->execute();
+                $rows = $statement->fetchAll(PDO::FETCH_OBJ);
+                foreach ($rows as $row) {
+                    $wiki = $wikisByDbname[$row->dbname];
+                    $wiki->_editcount = intval($row->counter);
+                    if ($row->counter > 0) {
+                        $globalEditCount += $row->counter;
+                        $wikisWithEditcount[$row->dbname] = $wiki;
                     }
                 }
-                unset($con);
+                unset($statement);
             }
         }
-        $this->alloverCount = array_sum($return);
-        $this->wikisWithEditscount = $wikisWithEditCount;
-        return $return;
+        $this->globalEditCount = $globalEditCount;
+        return $wikisWithEditcount;
     }
 
     /**
-     * returns centralauth information
+     * Get centralauth information
      * @staticvar null $centralauthData
      * @param string $dbname
-     * @return array|null|bool False if no centralauth
+     * @return object|null|bool False if no centralauth
      */
     private function _getCentralauthData($dbname) {
         static $centralauthData = null;
+        if ($this->isIP || $this->options['isPrefixPattern']) {
+            return false;
+        }
         if ($centralauthData === null) {
             $centralauthData = array();
-            $db = $this->app->getDB('centralauth', 'centralauth.labsdb');
-            $qry = $db->prepare('SELECT * FROM localuser WHERE lu_name = :luName;');
-            $qry->bindParam(':luName', $this->username, PDO::PARAM_STR);
-            $qry->execute();
-            $rows = $qry->fetchAll(PDO::FETCH_ASSOC);
-            unset($qry);
+            $pdo = $this->app->getDB('centralauth', 'centralauth.labsdb');
+            $statement = $pdo->prepare('SELECT * FROM localuser WHERE lu_name = :user;');
+            $statement->bindParam(':user', $this->user);
+            $statement->execute();
+            $rows = $statement->fetchAll(PDO::FETCH_OBJ);
+            unset($statement);
             if (!$rows) {
                 return false;
             }
             foreach ($rows as $row) {
-                $centralauthData[$row['lu_wiki']] = $row;
+                $centralauthData[$row->lu_wiki] = $row;
             }
         }
-        if (!key_exists($dbname, $centralauthData)) {
+        if (!isset($centralauthData[$dbname])) {
             return null;
         }
         return $centralauthData[$dbname];
     }
 
     /**
-     * Gibt die Daten zurück
+     * Add IP address to hostname map (if not already).
+     */
+    private function addIP( $ip ) {
+        if (!isset($this->hostnames[$ip])) {
+            $hostname = @gethostbyaddr($ip);
+            $this->hostnames[$ip] = $hostname ?: false;
+        }
+
+        return $this->hostnames[$ip] !== false;
+    }
+
+    /**
+     * Get collected data
+     *
      * @return array
      */
     public function getData() {
-        return $this->wikis;
+        return $this->datas;
     }
 
-    public function getUsername() {
-        return $this->username;
+    /**
+     * @return int
+     */
+    public function getWikiCount() {
+        return count($this->wikis);
     }
 
-    public function getHostname() {
-        return $this->hostname;
+    /**
+     * @return int
+     */
+    public function getResultWikiCount() {
+        return count($this->datas);
     }
 
-    public function getAlloverCount() {
-        return $this->alloverCount;
+    /**
+     * Get hostnames of searched IP(s).
+     *
+     * If IP was not found, or the search for was a user name or pattern,
+     * an empty array is returned.
+     *
+     * @return array
+     */
+    public function getHostnames() {
+        return $this->hostnames;
+    }
+
+    public function getGlobalEditcount() {
+        return $this->globalEditCount;
     }
 }

app.php
@@ -15,8 +15,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-class lb_app
-{
+class lb_app {
     private $times = array();
     private $clusters = array();
 
@@ -28,7 +27,7 @@ class lb_app
      * @return PDO
      */
     private function openDB($database = 'wikidatawiki', $cluster = null) {
-        $this->aTP('Open new Connection to '.$cluster);
+        $this->aTP('Create connection to '.$cluster);
         if (is_string($database)) {
             $host = $database.'.labsdb';
             $dbname = $database.'_p';
@@ -43,11 +42,11 @@ class lb_app
             $dbname = (is_string($database)) ? $database.'_p': 'information_schema';
         }
 
-        //Verbindung aufbauen
         try {
+            // Establish connection
             $pdo = new PDO('mysql:host='.$host.';dbname='.$dbname.';', settings::getSetting('user'), settings::getSetting('password'));
         } catch (PDOException $e) {
-            throw new lb_Exception("Database error! can't connect to ".  htmlspecialchars($dbname));
+            throw new lb_Exception('Database error: Unable to connect to '.  $dbname);
         }
         return $pdo;
     }
@@ -60,27 +59,22 @@ class lb_app
      * @return PDO
      */
     public function getDB($database = 'meta', $clusterNr = 's1.labsdb') {
-        $con = null;
 
         if (!$clusterNr) {
-            throw new lb_Exception("try to open DB without cluster specification");
+            throw new lb_Exception('Invalid DB cluster specification');
         }
         // Bereits vorhanden?
-        if (key_exists($clusterNr, $this->clusters)) {
-            $con = $this->clusters[$clusterNr];
-        } else {
-            // Datenbankverbindung öffnen
-            $con = $this->openDB($database, $clusterNr);
+        if (!isset($this->clusters[$clusterNr])) {
+            $this->clusters[$clusterNr] = $this->openDB($database, $clusterNr);
         }
+        $pdo = $this->clusters[$clusterNr];
+
         // Datenbank auswählen
-        $m = $con->prepare('USE `'.$database.'_p`;');
+        $m = $pdo->prepare('USE `'.$database.'_p`;');
         $m->execute();
         unset($m);
 
-        // Verbindung ablegen
-        $this->clusters[$clusterNr] = $con;
-
-        return $con;
+        return $pdo;
     }
 
     public function aTP($text) {
@@ -88,114 +82,96 @@ class lb_app
     }
 
     public function printTimes() {
-        $this->aTP("Finish");
+        print nl2br(htmlspecialchars($this->getTimes()));
+    }
+
+    public function getTimes() {
+        $this->aTP('Finish');
         $timebefore = null;
         $first = null;
+        $out = '';
         foreach ($this->times as $nr => $data) {
 
             $diff = ($timebefore === null) ? 0.0 : $data[0] - $timebefore;
             if ($timebefore === null) {
                 $first = $data[0];
             }
-            print('+' . round($diff, 2) . 's: ' . $data[1] . '<br />');
+            $out .= '+' . round($diff, 2) . 's: ' . $data[1] . "\n";
 
             $timebefore = $data[0];
         }
-        print('<br />Allover:'.round((microtime(true) - $first), 2).'s.<br />');
-        print('the time now is '.date('r'));
+        $out .= "\nTotal: ".round((microtime(true) - $first), 2)."s.\n";
+        $out .= 'Current time: '.date('r');
+        return $out;
     }
 
 
     /**
      * Take a mediawiki timestamp and returns a unix timestamp
      * @param string $tstime
-     * @return int unixtimestamp
+     * @return int UNIX timestamp
      */
     public function TsToUnixTime($tstime) {
-        $regYear  = substr($tstime, 0, 4);
-        $regMonth = substr($tstime, 4, 2);
-        $regDay   = substr($tstime, 6, 2);
-        $regHour  = substr($tstime, 8, 2);
-        $regMin   = substr($tstime, 10, 2);
-        $regSec   = substr($tstime, 12, 2);
+        // Based on MWTimestamp::setTimestamp for TS_MW
+        $da = array();
+        preg_match( '/^(\d{4})(\d\d)(\d\d)(\d\d)(\d\d)(\d\d)$/D', $tstime, $da );
 
-        return mktime($regHour, $regMin, $regSec, $regMonth, $regDay, $regYear);
+        $da = array_map( 'intval', $da );
+        $da[0] = '%04d-%02d-%02dT%02d:%02d:%02d.00+00:00';
+        $strtime = call_user_func_array( 'sprintf', $da );
 
+        #$date = new DateTime( $strtime, new DateTimeZone( 'GMT' ) );
+        #return $date->format($timeformat);
+
+        return strtotime($strtime);
     }
 
     /**
-     * returns a formated timestamp
+     * Take a mediawiki timestamp and convert to a different format
      * @param string $tstime
-     * @param array $months
      * @return string
      */
-    public function TStoUserTime($tstime, $timeformat, $months) {
-        if ($tstime == "infinity") {
+    public function TStoUserTime($tstime, $timeformat = 'H:i, d M Y') {
+        if ($tstime === 'infinity') {
             return $tstime;
-        } else {
-            $regYear = substr($tstime, 0, 4);
-            $regMonth = substr($tstime, 4, 2);
-
-            $tempdates = array("01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12");
-            $regMonthW = str_replace($tempdates, $months, $regMonth);
-
-            $regDay = substr($tstime, 6, 2);
-            $regHour = substr($tstime, 8, 2);
-            $regMin = substr($tstime, 10, 2);
-            $regSec = substr($tstime, 12, 2);
-
-            /*  variables:
-            Y = year,   eg. "2008"
-            m = month,  eg. "02"
-            F = month,  eg. "February"
-            d = day,    eg. "13"
-            H = hour,   eg. "15"
-            i = minute, eg. "08"
-            s = second, eg. "10"
-            */
-
-            return str_replace(array('Y','m','F','d','H','i','s'), array($regYear, $regMonth, $regMonthW, $regDay, $regHour, $regMin, $regSec), $timeformat);
-
-
         }
+        $time = $this->TsToUnixTime($tstime);
+        return date($timeformat);
     }
 
     /**
-     * parses the summary of contributions
-     * @param type $sum
-     * @param type $page
-     * @param type $project
-     * @return type
+     * Parse the edit summary of a user contribution
+     *
+     * @param string $sum
+     * @param string $page
+     * @param string $server
+     * @return string HTML
      */
-    public function wikiparser($sum, $page, $project)
-    {
-
+    public function wikiparser($sum, $page, $server)  {
         $page = htmlspecialchars($page, ENT_QUOTES);
         $sum = htmlspecialchars($sum);
-        //   Sektionen /* SEKTION */ parsen
-        $sum =preg_replace("/\/\*\s(.*)\s\*\/(.*)$/e", "'<span class=\'autocomment\'><a href=\'//$project/w/index.php?title=$page#'._wpsectdecode('\\1').'\'>→</a> \\1'.((strlen(trim('$2'))>2)?':':'').'</span>'", $sum);
+        // Parse "/* Section */"
+        $sum = preg_replace("/\/\*\s(.*)\s\*\/(.*)$/e", "'<span class=\'autocomment\'><a href=\'$server/w/index.php?title=$page#'._wpsectdecode('\\1').'\'>→</a> \\1'.((strlen(trim('$2'))>2)?':':'').'</span>'", $sum);
 
-        //Interne Links parsen
-        //[[LINK]]
-        //nichts ersetzen falls es ein | enthält
-        $sum =preg_replace("/\[\[([^\|\[\]]*)\]\]/e", "'<a href=\'//$project/w/index.php?title='._wpurldecode('\\1').'\'>\\1</a>'", $sum);
+        // Parse internal links "[[Link]]"
+        // Skip links with piped labels for later ("|")
+        $sum = preg_replace("/\[\[([^\|\[\]]*)\]\]/e", "'<a href=\'$server/w/index.php?title='._wpurldecode('\\1').'\'>\\1</a>'", $sum);
 
-        //[LINK|BESCHREIBUNG]]
-        $sum =preg_replace("/\[\[([^\|\[\]]*)\|{1}([^\|\[\]]*)\]\]/e", "'<a href=\'//$project/w/index.php?title='._wpurldecode('\\1').'\'>\\2</a>'", $sum);
+        // Parse internal links with labels "[[Link|Label]]"
+        $sum = preg_replace("/\[\[([^\|\[\]]*)\|{1}([^\|\[\]]*)\]\]/e", "'<a href=\'$server/w/index.php?title='._wpurldecode('\\1').'\'>\\2</a>'", $sum);
 
         return $sum;
     }
 
     /**
-     * Returns the Name of a Namespace, performs a api request if not already cached.
-     * @staticvar string $cache
+     * Returns the Name of a Namespace, performs an api request if not already cached.
      * @param int $id
      * @param string $dbName
-     * @param string $url
+     * @param string $server
      * @return string Namespace name
      */
-    public function getNamespaceName($id, $dbName, $url) {
-        static $cache = '';
+    public function getNamespaceName($id, $dbName, $server) {
+        static $cache = null;
         $id = intval($id);
 
         // Initialize cache
@@ -207,18 +183,15 @@ class lb_app
             }
         }
 
-        // Wiki exist in cache?
-        if (key_exists($dbName, $cache)) {
-            // Namespace exist in cache?
-            if (key_exists($id, $cache[$dbName])) {
-                return $cache[$dbName][$id];
-            }
+        // Wiki and namespace exist in cache?
+        if (isset($cache[$dbName][$id])) {
+            return $cache[$dbName][$id];
         }
 
         // Get information from API
-        $apiData = $this->apiRequest($url, array('meta' => 'siteinfo', 'siprop' => 'namespaces'));
+        $apiData = $this->apiRequest($server, array('meta' => 'siteinfo', 'siprop' => 'namespaces'));
         if (!is_array($apiData['query']['namespaces'])) {
-            throw new Exception('Error on namespace resolution for '.$url);
+            throw new Exception('Unable to retrieve namespaces from '.$server);
         }
 
         $cache[$dbName] = array();
@@ -226,29 +199,30 @@ class lb_app
             $cache[$dbName][intval($ns['id'])] = $ns['*'];
         }
 
-        //save cache
+        // Save cache
         file_put_contents(settings::getSetting('cacheFile'), json_encode($cache));
 
-        //return namespace name
-        if (!key_exists($dbName, $cache) || !key_exists($id, $cache[$dbName])) {
-            throw new Exception('Unknown namespace numer '.$id.' for '.$url);
+        if (!isset($cache[$dbName][$id])) {
+            throw new Exception('Unknown namespace number '.$id.' for '.$server);
         }
+
+        // Return namespace name
         return $cache[$dbName][$id];
     }
 
 
     /**
      * Performs a api request (post)
-     * @param string $url
+     * @param string $server
      * @param array $params
      * @return array or string
      */
-    public function apiRequest($url, $params) {
+    public function apiRequest($server, $params) {
         if (!is_array($params)) {
-            throw new Exception('invalid api parameters.');
+            throw new Exception('Invalid api parameters.');
         }
-        $url = 'https://'.$url.'/w/api.php';
-        //set defaults
+        $url = $server.'/w/api.php';
+        // Set defaults
         if (!key_exists('format', $params)) {
             $params['format'] = 'json';
         }

commit 12f34e308935aef03de0df6a67124e51a55f965b

guc: Fix broken display of projects with non-zero edits
Currently it says "X edits in 1 projects" always.

Regressed in beb5d5bfc4b, which created getResultWikiCount().
The reason it didn't work was that 'count()' is meant for arrays.
`$this->datas`, however, is an object.

Bug: T118662
Change-Id: Ia38d4b45757af9ffaba3a8d09304ad3b21a30640

lb/guc.php
@@ -273,7 +273,7 @@ class guc {
      * @return int
      */
     public function getResultWikiCount() {
-        return count($this->datas);
+        return count($this->wikisWithEditcount);
     }
 
     /**
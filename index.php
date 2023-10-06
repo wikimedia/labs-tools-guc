44c797855f54d111bd729fb97066f61786673ca7
Timo Tijhof <krinkle@fastmail.com>
2023-10-06 16:24:34 -0700

    build: Upgrade dev packages, require PHP 7.4+
    Change-Id: I34efa015d4faa65c2937aa40b6ed51a9f3699d7f

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ use Krinkle\Toolbase\HtmlSelect;
 require_once __DIR__ . '/vendor/autoload.php';
 
 error_reporting(E_ALL);
-ini_set('display_errors', 1);
+ini_set('display_errors', '1');
 date_default_timezone_set('UTC');
 
 $data = new stdClass();
